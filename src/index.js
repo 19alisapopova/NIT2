431c90e5e063ff13b1d3ab0e254f0259a721175d
Alisa <19alisapopova@gmail.com>
2018-11-26 21:19:34 +0200

    Test JS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import './scss/main.scss';
 
-console.log('Hello!');
-console.log(`The time is ${new Date()}`);
+let arr = [];
+
+$.ajax({
+    url: 'https://nit.tron.net.ua/api/category/list',
+    method: 'get',
+    dataType: 'json',
+    success: function(json){
+        console.log('Loaded via AJAX!');
+        for (let i = 0; i < json.length; i++){
+            arr.push(Object.create(json[i]));
+        }
+    },
+    error: function(xhr){
+        alert("An error occured: " + xhr.status + " " + xhr.statusText);
+    },
+});
+
+
+let s = "";
+for (let i in arr) {
+    s += "<li>" + arr[i].name + "</li>";
+}
+$(document.getElementById("navbarCategories")).html(s);
+
+
+//class='list-inline-item list-inline sticky-list-item m-5'
